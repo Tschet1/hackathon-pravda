4f1ca9b3498fb4ae8648480374bc4647f05da7c3
DomLeMarin <christianraemy@gmail.com>
2018-09-15 16:02:19 +0200

    Contract: Simple interaction of uPort with a smart contract, entering simply the value 13 for test

---
+++ uPort/example/interactWithContract.js
@@ -0,0 +1,122 @@
+/*jslint es6 */
+
+const uportConnect = require('uport-connect');
+const qrcode = require('qrcode-terminal');
+
+const mnidAddress = '2of2kCbrQbhhS5i3kQByM5w1d4b6PD8Y1xe';
+const signingKey = 'c7c478119b3048207e813f61c652b3f23849c3bf4c85aa37bd565ad16e70f3d4';
+const appName = 'SRF Digital ID';
+const contractAbi = [
+  { "constant": false,
+  "inputs": [
+  {
+    "name": "hash",
+    "type": "uint256"
+  }
+],
+  "name": "create",
+  "outputs": [],
+  "payable": false,
+  "stateMutability": "nonpayable",
+  "type": "function"
+},
+{
+  "constant": true,
+  "inputs": [
+  {
+    "name": "",
+    "type": "uint256"
+  }
+],
+  "name": "articles",
+  "outputs": [
+  {
+    "components": [
+      {
+        "name": "score",
+        "type": "uint8"
+      }
+    ],
+    "name": "metadata",
+    "type": "tuple"
+  },
+  {
+    "name": "exists",
+    "type": "bool"
+  }
+],
+  "payable": false,
+  "stateMutability": "view",
+  "type": "function"
+}
+];
+const contractAddress = '0x9a9f619c9ed10831f68493181e4152e9abcec34c';
+
+const uriHandler = (uri) => {
+  qrcode.generate(uri, {small: true})
+  console.log(uri)
+};
+
+const uport = new uportConnect.Connect(appName, {
+  uriHandler,
+  clientId: mnidAddress,
+  network: 'rinkeby',
+  signer: uportConnect.SimpleSigner(signingKey)
+});
+
+const web3 = uport.getWeb3();
+const myContractABI = web3.eth.contract(contractAbi);
+const myContract = myContractABI.at(contractAddress);
+
+// Request credentials
+uport.requestCredentials({
+  requested: ['name'],  verified: ['digitalid'],
+  }).then((credentials) => {
+  console.log(credentials);
+
+  var decodedId = uportConnect.MNID.decode(credentials.address);
+  var specificNetworkAddress = decodedId.address;
+
+  myContract.create(13, (error, txHash) => {
+    if (error) {
+      throw error;
+    }
+    waitForMined(txHash, { blockNumber: null }, // see next area
+    function pendingCB () {
+    // Signal to the user you're still waiting
+    // for a block confirmation
+    console.log("Waiting for feedback");
+  },
+    function successCB (data) {
+      // Great Success!
+      console.log("Likely you'll call some eventPublisherMethod(txHash, data)");
+    });
+  });
+
+
+});
+
+// Callback handler for whether it was mined or not
+const waitForMined = (txHash, response, pendingCB, successCB) => {
+  if (response.blockNumber) {
+    successCB()
+  } else {
+    pendingCB()
+    pollingLoop(txHash, response, pendingCB, successCB)
+  }
+}
+
+// Recursive polling to do continuous checks for when the transaction was mined
+const pollingLoop = (txHash, response, pendingCB, successCB) => {
+  setTimeout(function () {
+    web3.eth.getTransaction(txHash, (error, response) => {
+      if (error) { throw error }
+      if (response === null) {
+        response = { blockNumber: null }
+      } // Some ETH nodes do not return pending tx
+      waitForMined(txHash, response, pendingCB, successCB)
+    })
+  }, 1000) // check again in one sec.
+}
+
+
